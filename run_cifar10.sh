d9a429c55112db7a0d2451712d0c069ed9abbd3f
yuxwind <yuxwind@gmail.com>
2018-06-06 18:39:52 -0600

    add scripts to repeat our experiments

---
+++ run_cifar10.sh
@@ -0,0 +1,17 @@
+# Example for training and inference on compressed models
+#   run epsilon ResNet 110 with epsilon=2.5
+python cifar10-epsilon-resnet.py --gpu 2 -n 18 -e 2.5 -o cifar10-e_2.5-n_18 --cifar10
+#   compress the model of step 303420
+python compressModel.py --dir train_log.cifar10-e_2.5-n_18 --step 303420
+#   do inference on the compressed model of step 303420
+python cifar10-compressed-resnet.py --cfg train_log.cifar10-e_2.5-n_18/cifar10-epsilon-resnet/compressed_model_303420.cfg --gpu 2 --cifar10 
+
+# Other experiments to train on Cifar10
+#   run epsilon ResNet 200 with epsilon=2.5
+python cifar10-epsilon-resnet.py --gpu 2 -n 33 -e 2.5 -o cifar10-e_2.5-n_33 --cifar10
+#   run epsilon ResNet 500 with epsilon=2.5
+python cifar10-epsilon-resnet.py --gpu 2 -n 83 -e 2.5 -o cifar10-e_2.5-n_83 --cifar10
+#   run epsilon ResNet 750 with epsilon=2.5
+python cifar10-epsilon-resnet.py --gpu 2 -n 125 -e 2.5 -o cifar10-e_2.5-n_125 --cifar10
+
+# Please change the parameter to compress other model and do inference
---
+++ run_imagenet.sh
@@ -0,0 +1,23 @@
+# Example for training and inference on compressed models
+#   run epsilon-ResNet 101 with epsilon=2
+python imagenet-epsilon-resnet.py --gpu 0,1,2,3 --data /scratch/yuxwind/tensorpack_data/ilsvrc12_data/ -d 101 -e 2.0 
+#   compress the model of step 550000
+mv train_log train_log.imagenet-e_2-d_101
+python compressModel.py --dir train_log.imagenet-e_2-d_101 --step 550000
+#   do inference on the compressed model of step 550000
+python imagenet-compressed-resnet.py --cfg train_log.imagenet-e_2-d_101/imagenet-epsilon-resnet/compressed_model_550000.cfg --data /scratch/yuxwind/tensorpack_data/ilsvrc12_data/ --gpu 2  
+
+# Environment:
+#   All imangenet training experiments are on DGX machine with 8 Tesla V100-SXM2
+# Time:
+#   It takes about 86 hours to train epsilon-ResNet 101 with 4 GPUs
+#   It takes about 97 hours to train epsilon-ResNet 152 with 6 GPUs
+#   
+# run epsilon-ResNet 101 with epsilon=2.1
+python imagenet-epsilon-resnet.py --gpu 0,1,2,3 --data /scratch/yuxwind/tensorpack_data/ilsvrc12_data/ -d 101 -e 2.1 
+mv train_log train_log.imagenet-e_2.1-d_101
+# run epsilon-ResNet 152 with epsilon=1.8
+python imagenet-epsilon-resnet.py --gpu 0,1,2,3,4,5 --data /scratch/yuxwind/tensorpack_data/ilsvrc12_data/ -d 152 -e 1.8
+mv train_log train_log.imagenet-e_1.8-d_152
+
+# Please change the parameters to compress other models and do inference on it
